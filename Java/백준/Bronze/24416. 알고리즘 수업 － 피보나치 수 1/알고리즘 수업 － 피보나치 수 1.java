84a1d7d2ee39f35be40c230e09e66163dc2c8ed6
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-07-28 11:11:28 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 128 ms, Memory: 16124 KB -BaekjoonHub

---
+++ Java/백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N+1];
+
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i <= N; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        bw.write(dp[N] + " " + Math.max(0, N-2));
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
