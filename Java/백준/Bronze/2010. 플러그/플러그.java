2b31bd1e9f234546446178a93c7ef53a9fdd027f
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-08-05 00:46:49 +0900

    [Bronze III] Title: 플러그, Time: 240 ms, Memory: 41696 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2010. 플러그/플러그.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int result = 0;
+
+        for (int i = 0; i < N; i++) {
+            result += Integer.parseInt(br.readLine());
+        }
+
+        result -= (N-1);
+        bw.write(String.valueOf(result));
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
