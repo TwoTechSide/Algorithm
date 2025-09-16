b6aeff9359c28c9b6c23fb7cb7191dc1242ed720
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-09-16 15:10:29 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 772 ms, Memory: 120996 KB -BaekjoonHub

--- Java/백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ Java/백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -16,10 +16,10 @@ public class Main {
 
         int[][] dp = new int[N+1][N+1];
 
-        // [N+1][N+1] 크기의 표 -> 각 행마다 누적합 저장
+        // [N+1][N+1] 크기의 표 -> 누적합 연산
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) dp[i][j] = dp[i][j-1] + Integer.parseInt(st.nextToken());
+            for (int j = 1; j <= N; j++) dp[i][j] = dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] + Integer.parseInt(st.nextToken());
         }
 
         for (int i = 0; i < M; i++) {
@@ -30,13 +30,8 @@ public class Main {
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            int result = 0;
-
-            // x1 ~ x2에 대해 (y2의 합) - (y-1의 합) 계산
-            for (int j = x1; j <= x2; j++) {
-                result += dp[j][y2];
-                result -= dp[j][y1-1];
-            }
+            // dp[][]에 저장된 누적합으로 한 번에 계산
+            int result = dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1];
 
             bw.write(result+"\n");
         }
