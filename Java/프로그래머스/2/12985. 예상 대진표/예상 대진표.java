f03a09d58f534fe1043550ebc9c6cf8cce53915e
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-09-23 21:33:08 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 72.5 MB -BaekjoonHub

--- Java/프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ Java/프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,16 +2,6 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        a -= 1;
-        b -= 1;
-        int answer = 0;
-        
-        while (a != b) {
-            a >>= 1;
-            b >>= 1;
-            answer++;
-        }
-
-        return answer;
+        return Integer.toBinaryString((a-1)^(b-1)).length();
     }
 }
