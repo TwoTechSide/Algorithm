e8b123dc1accdc0d9bcd91c3b298af2704d727a3
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-09-30 23:14:45 +0900

    [Silver IV] Title: 국영수, Time: 1020 ms, Memory: 66636 KB -BaekjoonHub

--- Java/백준/Silver/10825. 국영수/국영수.java
+++ Java/백준/Silver/10825. 국영수/국영수.java
@@ -1,17 +1,24 @@
 import java.io.*;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
         String[][] arr = new String[N][4];
 
-        for (int i = 0; i < N; i++)
-            arr[i] = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = st.nextToken();
+            arr[i][1] = st.nextToken();
+            arr[i][2] = st.nextToken();
+            arr[i][3] = st.nextToken();
+        }
 
         Arrays.sort(arr, (o1, o2) -> {
             if (!o1[1].equals(o2[1])) return Integer.parseInt(o2[1]) - Integer.parseInt(o1[1]);
