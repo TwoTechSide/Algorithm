e66a422322269e3baa7eb9502d7cf809ef7399d6
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-09-30 23:26:44 +0900

    [Silver IV] Title: 국영수, Time: 612 ms, Memory: 59076 KB -BaekjoonHub

--- Java/백준/Silver/10825. 국영수/국영수.java
+++ Java/백준/Silver/10825. 국영수/국영수.java
@@ -1,8 +1,31 @@
 import java.io.*;
-import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    public static class Student implements Comparable<Student> {
+        String name;
+        int korea;
+        int english;
+        int math;
+
+        public Student(String name, int korea, int english, int math) {
+            this.name = name;
+            this.korea = korea;
+            this.english = english;
+            this.math = math;
+        }
+
+        @Override
+        public int compareTo(Student o) {
+            if (korea != o.korea) return o.korea - korea;
+            if (english != o.english) return english - o.english;
+            if (math != o.math) return o.math - math;
+            return name.compareTo(o.name);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,25 +33,23 @@ public class Main {
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        String[][] arr = new String[N][4];
+
+        PriorityQueue<Student> pq = new PriorityQueue<>();
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            arr[i][0] = st.nextToken();
-            arr[i][1] = st.nextToken();
-            arr[i][2] = st.nextToken();
-            arr[i][3] = st.nextToken();
+            pq.add(new Student(
+                    st.nextToken(),
+                    Integer.parseInt(st.nextToken()),
+                    Integer.parseInt(st.nextToken()),
+                    Integer.parseInt(st.nextToken())
+            ));
         }
 
-        Arrays.sort(arr, (o1, o2) -> {
-            if (!o1[1].equals(o2[1])) return Integer.parseInt(o2[1]) - Integer.parseInt(o1[1]);
-            else if (!o1[2].equals(o2[2])) return Integer.parseInt(o1[2]) - Integer.parseInt(o2[2]);
-            else if (!o1[3].equals(o2[3])) return Integer.parseInt(o2[3]) - Integer.parseInt(o1[3]);
-            else return o1[0].compareTo(o2[0]);
-        });
-
-        for(String[] subArr: arr)
-            bw.write(subArr[0] + "\n");
+        while(!pq.isEmpty()) {
+            Student s = pq.poll();
+            bw.write(s.name + "\n");
+        }
 
         bw.flush();
 
