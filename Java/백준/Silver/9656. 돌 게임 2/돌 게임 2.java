228470f780eeb431c09601a7e26a165c23c61f24
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-11-09 23:22:29 +0900

    [Silver V] Title: 돌 게임 2, Time: 108 ms, Memory: 14232 KB -BaekjoonHub

---
+++ Java/백준/Silver/9656. 돌 게임 2/돌 게임 2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        int N = readInt();
+        
+        System.out.println((N & 1) == 0 ? "SK" : "CY");
+    }
+
+    public static int readInt() throws IOException {
+        int N = 0, c;
+        while ((c = System.in.read()) > 32)
+            N = N * 10 + c - '0';
+        return N;
+    }
+}
