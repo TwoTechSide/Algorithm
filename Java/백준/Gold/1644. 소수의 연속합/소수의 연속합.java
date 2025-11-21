4abab4bc6a825f2188a9ff90c9311b2cb1ee76f0
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-11-22 03:00:58 +0900

    [Gold III] Title: 소수의 연속합, Time: 1928 ms, Memory: 22860 KB -BaekjoonHub

--- Java/백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ Java/백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -18,29 +18,28 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		int N = readInt();
-		int[] arr = new int[N+1];
+
 		List<Integer> primes = new ArrayList<>();
-
 		for (int i = 2; i <= N; i++) {
-			if (isPrime(i)) {
-				primes.add(i);
-				arr[i]++;
-
-				int sum = i;
-				int prev = primes.size() - 2;
-
-				while (prev >= 0) {
-					sum += primes.get(prev--);
-
-					if (sum > N)
-						break;
-
-					arr[sum]++;
-				}
-			}
+			if (isPrime(i)) primes.add(i);
 		}
 
-		System.out.println(arr[N]);
+		// 투 포인터로 풀이
+		int left = 0, right = 0;
+		int sum = 0;
+		int count = 0;
+
+		while (left <= right) {
+			if (sum >= N) {
+				if (sum == N) count++;
+				sum -= primes.get(left++);
+			} else {
+				if (right == primes.size()) break;
+				sum += primes.get(right++);
+			}
+		}
+		
+		System.out.println(count);
 	}
 
 	public static int readInt() throws IOException {
