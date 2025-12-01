9360182542d5ab5f4b56034f864ce5bf8b42c4b1
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-12-01 22:11:32 +0900

    [Bronze IV] Title: 10!, Time: 104 ms, Memory: 14132 KB -BaekjoonHub

---
+++ Java/백준/Bronze/28352. 10！/10！.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int result = 6;
+
+		while (N > 10) result *= N--;
+		System.out.println(result);
+	}
+}
