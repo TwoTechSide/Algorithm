807b168524a43210ccf7b11cdebdeb60afe3bd5a
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2026-09-01 20:21:28 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string, string is_prefix) {
+    
+    if (my_string.length() < is_prefix.length())
+        return 0;
+    
+    for (int i = 0; i < is_prefix.length(); i++) {
+        if (my_string[i] != is_prefix[i])
+            return 0;
+    }
+    
+    return 1;
+}
