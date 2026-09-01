cf2bdaa98fbc942cae51d9695d1032188d0ad0ac
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2026-09-01 18:23:53 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.18 ms, Memory: 6.36 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> arr, int idx) {
+    
+    for (int i = idx; i < arr.size(); i++) {
+        if (arr[i]) return i;
+    }
+    
+    return -1;
+}
