17dd3c163ee49da6834cc6c96f7c7f798310946f
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2026-09-03 00:29:32 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 0.40 ms, Memory: 4.76 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    for (vector<int> query: queries) {
+        int s = query[0];
+        int e = query[1];
+        int k = query[2];
+        
+        for (int i = s; i <= e; i++) {
+            if ((i % k) != 0) {
+                continue;
+            }
+            
+            arr[i]++;
+        }
+    }
+    
+    return arr;
+}
