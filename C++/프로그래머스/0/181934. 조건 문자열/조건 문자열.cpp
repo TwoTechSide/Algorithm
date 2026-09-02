713ad927ca41e621ff66f098a37ab4c3dd6a2fc9
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2026-09-03 01:29:05 +0900

    [level 0] Title: 조건 문자열, Time: 0.02 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181934. 조건 문자열/조건 문자열.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string ineq, string eq, int n, int m) {
+    int answer = 0;
+    string cond = ineq.append(eq);
+    
+    if (cond == "<=") return n <= m;
+    else if (cond == ">=") return n >= m;
+    else if (cond == "<!") return n < m;
+    else return n > m;
+}
