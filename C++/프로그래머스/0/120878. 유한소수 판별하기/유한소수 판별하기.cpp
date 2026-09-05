b6b30fbe4423e58d87aa40dc980db296ad248d7f
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2026-09-06 05:53:09 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    
+    for (int i = 2; i <= a; i++) {
+        while ((a % i == 0) && (b % i == 0)) {
+            a /= i;
+            b /= i;
+        }
+    }
+    
+    while (b % 2 == 0) b /= 2;
+    while (b % 5 == 0) b /= 5;
+    
+    return b == 1 ? 1 : 2;
+}
