67f0b4cc4f22ee9413278120b403ff64d9863b88
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2026-09-06 23:11:38 +0900

    [level 0] Title: 배열 조각하기, Time: 0.69 ms, Memory: 6.92 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181893. 배열 조각하기/배열 조각하기.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<int> query) {
+    
+    for (int i = 0; i < query.size(); i++) {
+        if ((i & 1) == 0) arr.resize(query[i]+1);
+        else arr.erase(arr.begin(), arr.begin() + query[i]);
+    }
+    
+    return arr;
+}
